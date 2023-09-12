6164d88dd4d389669bec5a6bfd20ae5e590309d7
andres.mera <feli041089@gmail.com>
2023-09-12 13:55:54 -0500

    Validacion con Vee-validate

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,5 +1,11 @@
-<script setup></script>
+<script setup>
+import { useForm, useField } from 'vee-validate';
 
+const { handleSubmit } = useForm();
+
+const email = useField('email');
+const password = useField('password');
+</script>
 <template>
     <v-card flat max-width="600" class="mx-auto my-10">
         <v-card-title class="text-h4 font-weigth-bold" tag="h3">
@@ -11,9 +17,9 @@
         </v-card-subtitle>
 
         <v-form class="mt-5">
-            <v-text-field type="email" label="Email" bg-color="blue-grey-lighten-5" />
+            <v-text-field type="email" label="Email" bg-color="blue-grey-lighten-5" v-model="email.value.value" />
 
-            <v-text-field type="password" label="Password" bg-color="blue-grey-lighten-5" />
+            <v-text-field type="password" label="Password" bg-color="blue-grey-lighten-5" v-model="password.value.value" />
 
             <v-btn block color="pink-accent-3">
                 Iniciar Sesión
