b40258e27f3a1d0a8574b29c569b454bf9275efa
andres.mera <feli041089@gmail.com>
2023-09-12 09:42:28 -0500

    Creando el formulario de Login

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,5 +1,23 @@
 <script setup></script>
 
 <template>
-    <h1 class="text-center text-h3 font-weight-bold my-5">Iniciar Sesión</h1>
+    <v-card flat max-width="600" class="mx-auto my-10">
+        <v-card-title class="text-h4 font-weigth-bold" tag="h3">
+            Iniciar Sesión
+        </v-card-title>
+
+        <v-card-subtitle class="text-h5">
+            Inicia Sesión con tu cuenta
+        </v-card-subtitle>
+
+        <v-form class="mt-5">
+            <v-text-field type="email" label="Email" bg-color="blue-grey-lighten-5" />
+
+            <v-text-field type="password" label="Password" bg-color="blue-grey-lighten-5" />
+
+            <v-btn block color="pink-accent-3">
+                Iniciar Sesión
+            </v-btn>
+        </v-form>
+    </v-card>
 </template>
