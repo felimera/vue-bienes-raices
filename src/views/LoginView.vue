613a672ec8a2869ab4e3fee4fe46309bea746814
andres.mera <feli041089@gmail.com>
2023-09-12 14:13:30 -0500

    Mostrando los mensajes de error en la validacion

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,10 +1,16 @@
 <script setup>
 import { useForm, useField } from 'vee-validate';
+import { loginSchema as validationSchema } from '../validation/loginSchema';
 
-const { handleSubmit } = useForm();
+const { handleSubmit } = useForm({ validationSchema });
 
 const email = useField('email');
 const password = useField('password');
+
+const submit = handleSubmit(() => {
+    console.log('Submit....');
+});
+
 </script>
 <template>
     <v-card flat max-width="600" class="mx-auto my-10">
@@ -17,11 +23,13 @@ const password = useField('password');
         </v-card-subtitle>
 
         <v-form class="mt-5">
-            <v-text-field type="email" label="Email" bg-color="blue-grey-lighten-5" v-model="email.value.value" />
+            <v-text-field type="email" label="Email" bg-color="blue-grey-lighten-5" class="mb-3" v-model="email.value.value"
+                :error-messages="email.errorMessage.value" />
 
-            <v-text-field type="password" label="Password" bg-color="blue-grey-lighten-5" v-model="password.value.value" />
+            <v-text-field type="password" label="Password" bg-color="blue-grey-lighten-5" class="mb-3"
+                v-model="password.value.value" :error-messages="password.errorMessage.value" />
 
-            <v-btn block color="pink-accent-3">
+            <v-btn block color="pink-accent-3" @click="submit">
                 Iniciar Sesión
             </v-btn>
         </v-form>
