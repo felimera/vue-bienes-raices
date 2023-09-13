9f39bfff92bed10835500d68fb0ebdc3761e59b9
andres.mera <feli041089@gmail.com>
2023-09-13 10:53:34 -0500

    Reestringir acceso a ciertas páginas con guards de navegación

--- src/router/index.js
+++ src/router/index.js
@@ -18,25 +18,41 @@ const router = createRouter({
       path: '/admin',
       name: 'admin',
       component: () => import('../views/admin/AdminLayout.vue'),
+      meta: { requiresAuth: true },
       children: [
         {
           path: '/admin/propiedades',
           name: 'admin-propiedades',
-          component: () => import('../views/admin/AdminView.vue')
+          component: () => import('../views/admin/AdminView.vue'),
+          meta: { requiresAuth: true },
         },
         {
           path: '/admin/nueva',
           name: 'nueva-propiedad',
-          component: () => import('../views/admin/NuevaPropiedadView.vue')
+          component: () => import('../views/admin/NuevaPropiedadView.vue'),
+          meta: { requiresAuth: true },
         },
         {
           path: '/admin/editar/:id',
           name: 'editar-propiedad',
-          component: () => import('../views/admin/EditarPropiedadView.vue')
+          component: () => import('../views/admin/EditarPropiedadView.vue'),
+          meta: { requiresAuth: true },
         },
       ]
     }
   ],
 });
+
+// Guard de navegación
+router.beforeEach((to, from, next) => {
+  const requiresAuth = to.matched.some(url => url.meta.requiresAuth);
+  if (requiresAuth) {
+    // Comprobar que el usuario este autenticado
+  }
+  else {
+    // No esta protegido por lo tanto mostramos la vista.
+    next();
+  }
+})
 
 export default router;
