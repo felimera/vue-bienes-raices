4b8bf5d257f62ac070efa94fc0be063852aa4da2
andres.mera <feli041089@gmail.com>
2023-09-14 10:23:40 -0500

    Mostrando el mapa de Leaflet en el formulario

--- src/views/admin/NuevaPropiedadView.vue
+++ src/views/admin/NuevaPropiedadView.vue
@@ -1,11 +1,15 @@
 <script setup>
+import { ref } from 'vue';
 import { useForm, useField } from 'vee-validate';
 import { collection, addDoc } from 'firebase/firestore';
 import { useFirestore } from 'vuefire';
 import { useRouter } from 'vue-router';
 import { validationSchema, imageSchema } from '../../validation/propiedadSchema';
 import useImage from '../../composables/useImage';
+import "leaflet/dist/leaflet.css";
+import { LMap, LTileLayer } from "@vue-leaflet/vue-leaflet";
 
+const zoom = ref(15);
 const items = [1, 2, 3, 4, 5];
 
 const { url, uploadImage, image } = useImage();
@@ -80,6 +84,13 @@ const submit = handleSubmit(async values => {
             <v-textarea class="mb-5" label="Descripción" v-model="descripcion.value.value"
                 :error-messages="descripcion.errorMessage.value"></v-textarea>
             <v-checkbox label="Alberca" v-model="alberca.value.value" :error-messages="alberca.errorMessage.value" />
+
+            <div style="height:600px; width:800px">
+                <l-map ref="map" v-model:zoom="zoom" :center="[47.41322, -1.219482]" :use-global-leaflet="false">
+                    <l-tile-layer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" layer-type="base"
+                        name="OpenStreetMap"></l-tile-layer>
+                </l-map>
+            </div>
 
             <v-btn color="pink-accent-3" block @click="submit"> Agregar Propiedad</v-btn>
         </v-form>
