a7bf8389deebc69089556b15787fff817c525e17
andres.mera <feli041089@gmail.com>
2023-09-15 08:15:34 -0500

    Mostar la fotografia actual o la nueva en el caso de actualizar.

--- src/views/admin/EditarPropiedadView.vue
+++ src/views/admin/EditarPropiedadView.vue
@@ -83,6 +83,17 @@ console.log(values);
 
             <div class="my-5">
                 <p class="font-weight-bold">Imagen Actual:</p>
+            <img
+                v-if="image"
+                            class="w-50"
+                            :src="image"
+            />
+            <img
+                v-else
+                            class="w-50"
+                            :src="propiedad?.imagen"
+            />
+
             </div>
 
 
